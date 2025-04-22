bfd8fda054c3c0924a03efc3d7a9a736ddc90cc8
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-04-22 04:14:41 +0000

    avance en ejercicios

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,5 +1,19 @@
 // Your code here
+function matrixBuilder(tamMatrix){
+    let builderArray = [];
+    for(let i=0; i<tamMatrix; i++){
+        
+        let rowMatrix = [];
 
+        for(let j=0; j<tamMatrix; j++){
+            let randonNumber = (Math.round(Math.random()));
+            rowMatrix.push(randonNumber);
+        }
+
+        builderArray.push(rowMatrix);
+    }
+    return builderArray
+}
 
 
 // Do not change anything from this line down
--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -6,6 +6,33 @@ let parkingState = [
 ]
 
 // Your code here
+function getParkingLotState(matrix) {
+  let totalSlots = 0;
+  let availableSlots = 0;
+  let occupiedSlots = 0;
 
+  for (let i = 0; i < matrix.length; i++) {
+    let rowParking = matrix[i];
+
+    for (let j = 0; j < rowParking.length; j++) {
+      if (rowParking[j] == 2) availableSlots += 1;
+      if (rowParking[j] == 1) occupiedSlots += 1;
+
+    }
+
+  }
+
+
+
+  totalSlots = 16 //matrix.length*rowParking.length;
+
+  parking = {
+    totalSlots: totalSlots,
+    availableSlots: availableSlots,
+    occupiedSlots: occupiedSlots
+  }
+
+  return parking;
+}
 
 console.log(getParkingLotState(parkingState))
